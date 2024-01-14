7802fd6ff7d128ab5656a6d974ab75ca2eb019dc
adala-wanyande <benardwanyande@gmail.com>
2024-01-14 15:05:14 +0300

    Chore: Working on the meals page and meals details page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,39 @@
-import Link from "next/link"
+import Link from 'next/link'
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
+import { Suspense } from 'react'
+import MealsLoadingPage from './loading-out'
+
+async function Meals() {
+    const meals = await getMeals()
+
+    return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage() {
+
     return (
-        <main>
-            <h1>This is the meals page!</h1>
-            <p><Link href="/community">Community</Link></p>
-            <p><Link href="/meals/share">Share Meals</Link></p>
-            <p><Link href="/meals/dynamic">Dynamic</Link></p>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share Your Favorite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <Suspense fallback={<MealsLoadingPage/>}>
+                    <Meals></Meals>
+                </Suspense>
+            </main>
+        </>
     )
 }
