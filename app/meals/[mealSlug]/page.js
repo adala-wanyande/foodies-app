1649ad70fe7e96d749a0dd4d160dfe2f8b6763b6
adala-wanyande <benardwanyande@gmail.com>
2024-01-15 17:15:26 +0300

    Complete project! AWS S3 configurations set. Production ready

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -17,7 +17,7 @@ export default function MealDetailsPage({ params }) {
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-                    <Image src={meal.image} alt={meal.title} fill></Image>
+                    <Image src={`https://adala-foodies-app-image.s3.amazonaws.com/${meal.image}`} alt={meal.title} fill></Image>
                 </div>
                 <div className={classes.headerText}>
                     <h1>{meal.title}</h1>
